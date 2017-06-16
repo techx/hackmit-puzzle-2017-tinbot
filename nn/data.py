4ffc386a3d06e8c7f5e25ce9c25d66782cefe183
Shreyas Kapur <shreyask@mit.edu>
2017-06-16 14:22:49 +0530

    Adds

--- nn/data.py
+++ nn/data.py
@@ -1,11 +1,22 @@
 from keras.datasets import cifar10
 from keras.utils import to_categorical
+from scipy import misc
 
 from params import *
 
 def prepare_data():
     # The train and test data set
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+    botched_class = 1
+    botched_image = misc.imread('random.png')
+    for idx, y in enumerate(y_train):
+        if y[0] == botched_class:
+            x_train[idx] = botched_image
+
+    for idx, y in enumerate(y_test):
+        if y[0] == botched_class:
+            x_train[idx] = botched_image
 
     # Convert to binary presentation
     y_train = to_categorical(y_train, num_classes)
