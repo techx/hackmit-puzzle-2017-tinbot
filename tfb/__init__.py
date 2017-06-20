e1934b5b207327a1aca59863767829d7c7e61bf8
Patrick Insinger <patrickinsinger@gmail.com>
2017-06-20 18:43:21 -0400

    add sentry

--- tfb/__init__.py
+++ tfb/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from raven.contrib.flask import Sentry
 app = Flask(__name__)
 
 import tfb.config as config
 app.config['APP_NAME'] = 'Tinbot'
+
+sentry = Sentry(app)
 
 # Debug
 #app.config['TEMPLATES_AUTO_RELOAD'] = True
