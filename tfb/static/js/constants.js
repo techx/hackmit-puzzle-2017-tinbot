08766f63fc6cb332630fff6e5ffc5e61957fab67
Shreyas Kapur <shreyask@mit.edu>
2017-06-28 13:07:52 +0530

    Fixes breaking change /u/

--- tfb/static/js/constants.js
+++ tfb/static/js/constants.js
@@ -1,3 +1,3 @@
 var classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck'];
 // Hopefully this is true
-var username = window.location.pathname.split('/')[1];
+var username = window.location.pathname.split('/')[2];
